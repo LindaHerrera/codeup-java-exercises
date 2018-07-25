7d2ef7bb07e8fa63a7b2d7bb1c268d4d4a18f22b
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-25 16:49:22 -0500

    end of java string exercise with solutions

--- src/Bob.java
+++ src/Bob.java
@@ -5,11 +5,18 @@ public class Bob {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("What did you eat for lunch?");
-        String response = sc.next();
-            if(response.equalsIgnoreCase("food")){
-                System.out.println(" \'Sure\'");
-            }
+        System.out.println("Write something");
+        String response = sc.nextLine().trim();
+
+        if(response.endsWith("?")) {
+            System.out.println("'Sure'");
+        } else if(response.endsWith("!") || response.toUpperCase().equals(response) && !response.isEmpty() ){
+            System.out.println("'Bruh...chillax'");
+        }else if (response.isEmpty()){
+            System.out.println("Fine. Be that way!");
+        }
+
+
 
 
     }
