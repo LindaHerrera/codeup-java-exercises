52e5fef9b3fd4b84f4e1a8ccdc75d6f1fb059c19
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-25 09:25:40 -0500

    java loops practice: first portion of exercises with while loop and for loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,26 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+
+        boolean condition = true;
+        int i = 5;
+
+        while(condition){
+            if(i == 15){
+                condition = false;
+            }
+            System.out.println("this is the while loop: "+ i);
+            i++;
+        }
+
+        for(int n = 5; n <=15; n++){
+            System.out.println("this is the for loop: " + n);
+            }
+
+
+
+
+
+
+    }
+}
