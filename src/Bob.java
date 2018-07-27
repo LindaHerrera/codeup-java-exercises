59dd5515e9a802a8d43b394e924f6a5b7a29918d
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-27 14:55:20 -0500

    played with Bob class code to get more understanding and created sandbox class to begin game

--- src/Bob.java
+++ src/Bob.java
@@ -3,20 +3,32 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
+    talkToBob("Hello");         //calling method
 
-        System.out.println("Write something");
-        String response = sc.nextLine().trim();
 
-        if(response.endsWith("?")) {
-            System.out.println("'Sure'");
-        } else if(response.endsWith("!") || response.toUpperCase().equals(response) && !response.isEmpty() ){
+
+    }
+
+    public static void talkToBob(String input){         //declare and initialize method
+
+        Scanner sc = new Scanner(System.in);            //set scanner
+
+        System.out.println("Write something to Bob");   //print question
+        String response = sc.nextLine().trim();         //declare variable to hold the input
+
+        if(response.endsWith("?")) {            // if input ends with ?
+            System.out.println("'Sure'");       //then do this
+
+        } else if(response.endsWith("!") || (response.toUpperCase().equals(response) && !response.isEmpty() )){ //ends with ! or upper case and is empty..
             System.out.println("'Bruh...chillax'");
-        }else if (response.isEmpty()){
-            System.out.println("Fine. Be that way!");
+
+        }else if (response.isEmpty()){      //is the input is empty
+            System.out.println("Fine. Be that way!");   //then do this
+
+        }else if (response.endsWith("Bob")){        //if input ends with his name
+            System.out.println("...");              //then do this
+
         }
-
-
 
 
     }
--- src/sandbox.java
+++ src/sandbox.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class sandbox {
 
     public static void main(String[] args){
+testRun("");
 
-        String name = "Linda";
-        System.out.println(name);
+    }
+    public static String testRun(String response) {         //declare and initialize method
 
+        Scanner sc = new Scanner(System.in);            //set scanner
+
+        System.out.println("Welcome");   //print question
+        System.out.println("Would you like to give me some of your time? [y/n]");
+        String input = sc.nextLine().trim().toLowerCase();         //declare variable to hold the input
+        if (input.endsWith("y")) {            // if input ends with ?
+            System.out.println("So gracious. Let's begin...");       //then do this
+        } else if (input.endsWith("n") ) { //ends with ! or upper case and is empty..
+            System.out.println("Pitty..");
+        } else if (input.isEmpty()) {      //is the input is empty
+            System.out.println("Fine. Be that way!");
+
+        }
+        return testRun("");
     }
 
 
