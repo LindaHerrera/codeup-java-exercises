26e8cd3482dfb28cadae1d44ddf9e98728d4b668
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-31 12:05:01 -0500

    random server generator name exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+
+    public static void main(String[]args){
+
+    String [] adjectives = new String[10];
+    adjectives[0] = "bitter";
+    adjectives[1] = "sharp";
+    adjectives[2] = "frightened";
+    adjectives[3] = "sweet";
+    adjectives[4] = "rusted";
+    adjectives[5] = "jealous";
+    adjectives[6] = "cuddly";
+    adjectives[7] = "drained";
+    adjectives[8] = "crumpled";
+    adjectives[9] = "shaggy";
+
+
+
+//    String [] adjectives = {"bitter", "sharp", "yellow", "sweet", "rusted", "hunched", "bleached", "drained", "crumpled","starved"};
+
+
+String [] nouns = new String[10];
+    nouns[0] = "notebook";
+    nouns[1] = "left shoe";
+    nouns[2] = "chat room";
+    nouns[3] = "glasses";
+    nouns[4] = "coffee cup";
+    nouns[5] = "nail polish";
+    nouns[6] = "solo cup";
+    nouns[7] = "car";
+    nouns[8] = "tattoo";
+    nouns[9] = "laser pointer";
+
+
+    int rndN = new Random().nextInt(nouns.length);
+    int rndA = new Random().nextInt(adjectives.length);
+        System.out.println(adjectives[rndA]+" "+nouns[rndN]);
+
+
+
+
+
+
+
+
+    }
+
+}
