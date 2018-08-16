842bff18ac685f1e5e9722c913b2eadc6ce4c9c0
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-08-16 10:51:27 -0500

    cleaning branch tree for grading period

--- src/util/FileIOPlay.java
+++ src/util/FileIOPlay.java
@@ -1,4 +1,16 @@
 package util;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 
 public class FileIOPlay {
+    public static void main(String[] args) throws IOException {
+        Files.createDirectory(Paths.get("./src/test"));
+
+//        Files.isDirectory();
+    }
 }
+
